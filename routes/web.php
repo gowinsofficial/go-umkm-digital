110c261ae5874daafa63563874573ce87e07c57e
moh imam wahyudi <m.imam.wahyudi@dwidasa.com>
2023-11-07 21:28:25 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/kuliner-madura', function () {
+    return view('kuliner');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
